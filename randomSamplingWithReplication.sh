f39c127cbfd89ce0d36fdf08b47f4b02181cb28e
Rafael <amauri.rafael33@gmail.com>
2024-12-29 04:12:44 -0300

    Improved comments and variable names

--- randomSamplingWithReplication.sh
+++ randomSamplingWithReplication.sh
@@ -6,14 +6,14 @@ n_replications=$1 # How many replications to run
 
 for i in 10 20; do 
 	for j in $(seq 1 $n_replications); do
-        FILENAME="phoenix$i-run$j"
+        currentFolder="phoenix$i-run$j"
 		
 		cd preprocess
-        echo $(pwd)
-		python3 dataset_preprocess.py --dataset $FILENAME --dataset-root ../dataset/$FILENAME/phoenix-2014-multisigner
+
+		python3 dataset_preprocess.py --dataset $currentFolder --dataset-root ../dataset/$currentFolder/phoenix-2014-multisigner
 
 		cd ../
 
-		python3 main.py --device 0 --dataset $FILENAME --loss-weights Slow=0.25 Fast=0.25 --work-dir work_dir/$FILENAME --batch-size 1 --num-epoch 1
+		python3 main.py --device 0 --dataset $currentFolder --loss-weights Slow=0.25 Fast=0.25 --work-dir work_dir/$currentFolder 
 	done; 
 done
