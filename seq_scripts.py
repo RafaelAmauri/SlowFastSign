Fix uncertainty calculation

seq_scripts.py
@@ -147,7 +147,7 @@ def seq_feature_generation(loader, model, device, mode, work_dir, recoder):
             save_file = {
                 "label": data[2][start:end],
                 "features": ret_dict['framewise_features'][sample_idx][:, :vid_lgt[sample_idx]].T.cpu().detach(),
-                "confidence": ret_dict["predConf"].item()
+                "confidence": ret_dict["predConf"]
             }
             np.save(filename, save_file)
             start = end

utils/decode.py
@@ -50,7 +50,8 @@ class Decode(object):
 
         # beam_scores is of shape [nBatch, nBeams]. The best beam is the the first one, the one in the 0th position.
         # beam_scores[..., 0] gets the best beam (the one in the 0th position) at each batch.
-        beamConfidence = 1 / np.exp(beam_scores[..., 0] )
+        beam_scores = beam_scores[..., 0].to(torch.float64)
+        beamConfidence = (1 / np.exp(beam_scores)).item()
 
         return ret_list, beamConfidence
 